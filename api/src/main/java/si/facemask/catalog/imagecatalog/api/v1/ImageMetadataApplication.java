35e5f36c787dc01c1d91cdf0dcf5d537e179629f
dextos658 <dd2008@student.uni-lj.si>
2021-01-01 18:44:07 +0100

    Add files via upload

---
+++ api/src/main/java/si/facemask/catalog/imagecatalog/api/v1/ImageMetadataApplication.java
@@ -0,0 +1,9 @@
+package si.facemask.catalog.imagecatalog.api.v1;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class ImageMetadataApplication extends Application {
+
+}
